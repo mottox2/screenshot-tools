db24f13be6366a05854e41057762039fb26890c4
mottox2 <y.mottox2@gmail.com>
2021-02-07 20:36:52 +0900

    Store data in sqlite

--- src/pages/api/pages.ts
+++ src/pages/api/pages.ts
@@ -1,5 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  const db = await open({
+    filename: "./sqlite.db",
+    driver: sqlite3.Database,
+  });
+  if (method === "GET") {
+    await db.run(`CREATE TABLE IF NOT EXISTS tbl (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT
+    )`);
+
+    const result = await db.all("SELECT * FROM tbl");
+    res.status(200).json({ items: result });
+  }
+
+  if (method === "POST") {
+    const result = await db.run(
+      "INSERT INTO tbl (content) VALUES (?)",
+      new Date().toISOString()
+    );
+    res.status(200).json({ lastID: result.lastID });
+  }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
+import axios from "axios";
+
 const IndexPage = () => {
-  return <p>Hello World</p>;
+  return (
+    <p>
+      Hello World
+      <button
+        onClick={async () => {
+          const res = await axios.post("/api/pages", {
+            data: "hoge",
+          });
+          console.log(res);
+        }}
+      >
+        Create
+      </button>
+    </p>
+  );
 };
 
 export default IndexPage;
