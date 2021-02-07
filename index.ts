a28667ef4b928c07de85034ae06b3f8c6b0900b2
mottox2 <y.mottox2@gmail.com>
2021-02-07 10:08:59 +0900

    Use arg to handle args

--- index.ts
+++ index.ts
@@ -1,18 +1,44 @@
 import path from "path";
+import arg from "arg";
 import puppeteer from "puppeteer-core";
 
-const capture = async () => {
+const args = arg({
+  "--out": String,
+  "--width": Number,
+  "--height": Number,
+  "-o": "--out",
+  "-w": "--width",
+  "-h": "--height",
+});
+
+type Options = {
+  width: number;
+  height: number;
+  out: string;
+};
+
+const capture = async (options: Options) => {
+  console.log(options);
+  const { width, height, out } = options;
   const browser = await puppeteer.launch({
     executablePath:
       "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
   });
   const page = await browser.newPage();
-  await page.goto(path.join("file:", __dirname, "index.html"));
+  await page.setViewport({ width, height });
+  await page.goto(path.join("file:", __dirname, out));
   await page.screenshot({ path: "example.png" });
 
   await browser.close();
 };
 
 (async () => {
-  await capture();
+  const out = args._[0];
+  if (!out) return;
+
+  await capture({
+    width: args["--width"] || 1080,
+    height: args["--height"] || 740,
+    out,
+  });
 })();
